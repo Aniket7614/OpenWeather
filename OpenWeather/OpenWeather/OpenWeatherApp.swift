f0c795003ecc1bd855dc940ee6449163918e214e
Aniket7614 <aniketk561@gmail.com>
2023-04-26 20:05:12 +0530

    Commit- Initial commit

---
+++ OpenWeather/OpenWeather/OpenWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherApp.swift
+//  OpenWeather
+//
+//  Created by Halcyon Tek on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
